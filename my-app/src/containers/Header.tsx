7dd686d0e3823826ddc5e27da0271b36f58d67ba
Karkar1ch <karina.rogalevich@email.com>
2024-10-23 15:19:06 +0200

    fix header

--- my-app/src/containers/Header.tsx
+++ my-app/src/containers/Header.tsx
@@ -7,27 +7,27 @@ import React from "react";
 const Header = () => {
     return(
         <header>
-            <nav className="z-10 shadow-navbar p-2 flex relative h-[100px] w-full">
+            <nav className="z-10 p-2 flex relative h-[100px] w-full">
                 <a href="/home" className="flex text-[32px] font-nav-logo ml-[100px] my-auto relative w-auto">Educational Platform</a>
                 <img src="/img/leaf.png" alt="" className="flex relative my-auto w-[50px] h-[50px]" />
                 <div className="flex relative my-auto text-[20px]">
                     <ul className="flex relative ml-[100px] w-auto">
                         <li className="mx-[20px]">
-                            <span className="flex relative">
+                            <button className="flex relative">
                                 <img src="/icons/category.png" className="w-[16px] h-[16px] mr-1 my-auto" alt="" />
                                 Catalog
                                 <img src="/icons/down-arrow.png" className="w-[18px] h-[18px] my-auto" alt="" />
-                            </span>
+                            </button>
                         </li>
                         <li className="mx-[20px]">
-                            <span className="flex relative ">
+                            <button className="flex relative ">
                                 About
-                            </span>
+                            </button>
                         </li>
                         <li className="mx-[20px]">
-                            <span className="flex relative ">
+                            <button className="flex relative ">
                                 Support
-                            </span>
+                            </button>
                         </li>
                     </ul>
                 </div>
@@ -40,11 +40,11 @@ const Header = () => {
                     </div>
                     <span className="flex items-center relative w-[2px] h-[30px] mx-[20px] bg-[#3415D0]"></span>
                     <div className={"flex relative themes items-center"}data-icon={'sun'}>
-                        <Sun/>
+                        <button><Sun/></button>
                     </div>
                     <div className="flex items-center realtive mx-[20px]">
                         <img src="/icons/earth-grid.png" className="w-[20px] h-[20px] my-auto" alt="" />
-                        <span>Eng</span>
+                        <button>Eng</button>
                         <img src="/icons/down-arrow.png" className="w-[16px] h-[16px] my-auto" alt="" />
                     </div>
                 </div>
