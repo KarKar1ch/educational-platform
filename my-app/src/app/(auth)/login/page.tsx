4e81606feccd332eec709654633bfacf577daddc
Karkar1ch <karina.rogalevich@email.com>
2024-10-31 18:36:25 +0200

    login

--- my-app/src/app/(auth)/login/page.tsx
+++ my-app/src/app/(auth)/login/page.tsx
@@ -6,6 +6,8 @@ import React, {useState} from "react"
 const LoginPage = () => {
     const [email, setEmail] = useState<string>()
     const [password, setPassword] = useState<string>()
+
+    
     return(
         <div>
 
