56ae2a2610f12b94f0f74ee3252c4086dd6a3f45
Karkar1ch <karina.rogalevich@email.com>
2024-11-03 17:28:44 +0200

    adding react-hook-orms

--- my-app/src/app/auth/page.tsx
+++ my-app/src/app/auth/page.tsx
@@ -1,20 +1,27 @@
+"use client"
 import React from "react";
-import Image from "next/image";
+import { useForm } from 'react-hook-form';
+
+import ImgAuth from "@/components/img/ImgAuth";
 
 const AuthPage = ()=>{
 
+    const {register, handleSubmit} = useForm()
+
+    const onSubmit = (data:any) => {
+        
+    }
 
     return(
-    <div>
-        <div></div>
-        <div className="w-[695px]">
-            <div className="w-[50%] p-[20px] inline-block">
-                <form className="">
+    <div  className="flex justify-center items-center h-screen">
+        <div className="w-[695px] h-[415px]">
+            <div className="w-[50%] p-[20px] inline-block ">
+                <form className="" onSubmit={handleSubmit(onSubmit)}>
                     <div>
                         <h1 className="font-bold text text-2xl text-center">Log In</h1>
                         <div className=" ">
-                            <button className="w-full bg-[#3E63F5] text-white rounded-3xl mb-[10px]"><Image src={''} alt=""/>Sign In with Instagram</button>
-                            <button className="w-full bg-[#3E63F5] text-white rounded-3xl "><Image src={''} alt=""/>Sign In with Google</button>
+                            <button className="w-full bg-[#3E63F5] text-white rounded-3xl mb-[10px]">Sign In with Instagram</button>
+                            <button className="w-full bg-[#3E63F5] text-white rounded-3xl ">Sign In with Google</button>
                         </div>
                     </div>
                     <div className="my-[10px]">
@@ -28,26 +35,27 @@ const AuthPage = ()=>{
                                 className="w-full h-[40px] border border-[#3E63F5] rounded-xl mb-[10px]"
                                 placeholder="email"
                                 type="text"
+                                {...register('email')}
                             />
                             <input  
                                 className="w-full h-[40px] border border-[#3E63F5] rounded-xl mb-[10px]"
                                 placeholder="password"
-                                type="password" 
+                                type="password"
+                                {...register('password')}
                             />
                         </div>
                         <button className = "">New to our Platform?</button>
                     </div>
-                    <div><button className="w-full bg-[#3E63F5] text-white rounded-3xl mb-[10px]">Log In</button></div>
+                    <div className="flex justify-center"><button className="w-[50%] bg-[#3E63F5] text-white rounded-3xl mt-[25px]" type="submit">Log In</button></div>
                 </form>
             </div>
             <div className="w-[50%] inline-block">
                 <div>
+                    <ImgAuth/>
                     <h2>Welcome to Educational Platform</h2>
                     <p>By continuing,you agree to our User Agreement and acknowledge tha you understand the Privacy Policy</p>
                 </div>
                 <div>
-                    <Image src={''} alt=""/>
-                    <Image src={''} alt=""/>
                 </div>
             </div>
         </div>
---
+++ my-app/src/components/img/ImgAuth.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Image from "next/image";
+
+const ImgAuth = () => {
+    return(
+        <Image 
+            src="/auth/Vector.png" 
+            alt="img"
+            width={330 }
+            height={415}
+        />
+    )
+}
+
+export default ImgAuth
